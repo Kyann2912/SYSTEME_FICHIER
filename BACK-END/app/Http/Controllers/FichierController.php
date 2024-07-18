bfaf5b8edf9d9dd2a4a6d0fcf67ff58f820157d9
Kyann2912 <kyann372@gmail.com>
2024-07-18 14:11:50 +0000

    AJOUT

--- BACK-END/app/Http/Controllers/FichierController.php
+++ BACK-END/app/Http/Controllers/FichierController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Fichier;
 
 use Illuminate\Http\Request;
 
@@ -13,22 +14,15 @@ class FichierController extends Controller
         $valider = $request->validate([
             'filiere' => 'required|string',
             'niveau' => 'required|string',
-            'chemin' => 'required|string|max:10240', // Exemple : fichier PDF, DOC, DOCX jusqu'à 10MB
+            'chemin' => 'required|string|max:10240',
         ]);
-
-        // Enregistrer le fichier
-        // $file = $request->file('file');
-        // $fileName = time() . '_' . $file->getClientOriginalName();
-        // $filePath = $file->storeAs('uploads', $fileName); // Stockage du fichier dans le dossier 'storage/app/uploads'
 
         // Créer une nouvelle entrée dans la base de données
         $fichier = new Fichier();
         $fichier->filiere = $valider['filiere'];
-        $fichier->filiere = $valider['niveau'];
-        $fichier->filiere = $valider['chemin'];
+        $fichier->niveau = $valider['niveau'];
+        $fichier->chemin = $valider['chemin'];
 
-        // $fichier->niveau = $request->niveau;
-        // $fichier->chemin = $request->chemin; // Chemin du fichier stocké
         $fichier->save();
 
         return response()->json(['message' => 'Fichier enregistré avec succès'], 201);
--- BACK-END/app/Models/Fichier.php
+++ BACK-END/app/Models/Fichier.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Fichier extends Model
 {
     use HasFactory;
+    protected $guarded = ['filiere','niveau','chemin'];
+
 }
